d48622a19162f683fdb67325315dd5d76e560338
Natalie Nguyen <uyenntm2001@gmail.com>
2021-09-08 19:02:04 -0700

    fix href

--- src/components/DegreeToggle.js
+++ src/components/DegreeToggle.js
@@ -12,13 +12,13 @@ const DegreeToggle = ({degreeType=constConfig.F, updateForecastDegree}) => {
     <React.Fragment>
     <div className="form-check form-check-inline pb-3">
         <label className="form-check-label" htmlFor={constConfig.C}>
-        <a href="#" className={"degree "+cClassName} id={constConfig.C} onClick={updateForecastDegree}
+        <a href="/#" className={"degree "+cClassName} id={constConfig.C} onClick={updateForecastDegree}
         value={constConfig.C}>°C</a>
         </label>
       </div> |
       <div className="form-check form-check-inline pl-3 pb-3">
         <label className="form-check-label" htmlFor={constConfig.F}>
-        <a href="#" className={"degree "+fClassName} id={constConfig.F} onClick={updateForecastDegree}
+        <a href="/#" className={"degree "+fClassName} id={constConfig.F} onClick={updateForecastDegree}
         value={constConfig.F}>°F</a>
         </label>
       </div>
